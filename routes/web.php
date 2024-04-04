830379b4b3f73d18b7836a3a06237685b3635f75
zakariae.aboud <zakariae.aboud@mavistro.com>
2024-04-04 12:50:16 +0000

    Redirect to /calculator from home

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\CalculatorController;
 use Illuminate\Support\Facades\Route;
 
+# Redirect to /calculator from home 
+Route::redirect('/', '/calculator', 301);
+
 # Display the calculator form
 Route::get('/calculator', [CalculatorController::class, 'index']);
 
